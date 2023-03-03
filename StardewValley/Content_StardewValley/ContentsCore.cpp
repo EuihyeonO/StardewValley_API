07844cce7f29694a9274758ad2186a0d65820f46
EuihyeonO <yuu5666@naver.com>
2023-03-03 16:06:26 +0900

    버그 수정 2차

--- StardewValley/Content_StardewValley/ContentsCore.cpp
+++ StardewValley/Content_StardewValley/ContentsCore.cpp
@@ -40,9 +40,9 @@ void ContentsCore::Start()
 	CreateLevel<Level_House>("House");
 	CreateLevel<Level_Mine>("Mine");
 
-	ChangeLevel("Road");
+	ChangeLevel("House");
 
-    CurLevel = "Road";   
+    CurLevel = "House";   
 }
 
 void ContentsCore::Update()
